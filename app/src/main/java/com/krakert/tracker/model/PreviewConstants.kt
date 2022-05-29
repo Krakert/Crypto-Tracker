998d672bcbf38782881f4b439f70e28c1c478722
Stefan de Kraker <stefandekraker1999@gmail.com>
2022-05-29 02:27:25 +0200

    I dont really get te @Preview annotation :sad:

---
+++ app/src/main/java/com/krakert/tracker/model/PreviewConstants.kt
@@ -0,0 +1,19 @@
+package com.krakert.tracker.model
+
+import android.content.res.Configuration
+
+const val WEAR_PREVIEW_ELEMENT_WIDTH_DP = 100
+const val WEAR_PREVIEW_ELEMENT_HEIGHT_DP = 100
+
+const val WEAR_PREVIEW_ROW_WIDTH_DP = 300
+const val WEAR_PREVIEW_ROW_HEIGHT_DP = 100
+
+const val WEAR_PREVIEW_DEVICE_WIDTH_DP = 214
+const val WEAR_PREVIEW_DEVICE_HEIGHT_DP = 214
+
+const val WEAR_PREVIEW_API_LEVEL = 26
+
+const val WEAR_PREVIEW_UI_MODE = Configuration.UI_MODE_TYPE_WATCH
+
+const val WEAR_PREVIEW_BACKGROUND_COLOR_BLACK: Long = 0x000000
+const val WEAR_PREVIEW_SHOW_BACKGROUND = true
