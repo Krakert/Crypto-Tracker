0518327dedd8e1e8c29d445cd1cd41fafd512f52
Stefan de Kraker <stefandekraker1999@gmail.com>
2022-06-03 12:54:47 +0200

    Close #1, forget to add parameter to the API call, marketData = true

--- app/src/main/java/com/krakert/tracker/repository/CoinGeckoRepository.kt
+++ app/src/main/java/com/krakert/tracker/repository/CoinGeckoRepository.kt
@@ -8,6 +8,8 @@ import com.krakert.tracker.model.Currency
 import com.krakert.tracker.model.DataDetailsCoin
 import com.krakert.tracker.model.MarketChart
 import drewcarlson.coingecko.CoinGeckoClient
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.withTimeout
 import java.util.*
 
@@ -47,22 +49,26 @@ class CoinGeckoRepository(context: Context) {
         }
     }
 
-    suspend fun getDetailsCoinByCoinId(coinId: String): DataDetailsCoin {
+    suspend fun getDetailsCoinByCoinId(coinId: String): Flow<DataDetailsCoin> {
         return try {
-            withTimeout(10_00) {
-                val data = coinGecko.getCoinById(coinId)
-                DataDetailsCoin(
-                    name = data.name,
-                    priceCurrent = data.marketData?.currentPrice?.get(currencyString) ?: 0.0,
-                    image = data.image,
-                    priceChangePercentage24h = data.marketData?.priceChangePercentage24h ?: 0.0,
-                    priceChangePercentage7d = data.marketData?.priceChangePercentage7d ?: 0.0,
-                    circulatingSupply = data.marketData?.circulatingSupply ?: 0.0,
-                    high24h = data.marketData?.high24h?.get(currencyString) ?: 0.0,
-                    low24h = data.marketData?.low24h?.get(currencyString) ?: 0.0,
-                    marketCap = data.marketData?.marketCap?.get(currencyString) ?: 0.0,
-                    marketCapChangePercentage24h = data.marketData?.marketCapChangePercentage24h ?: 0.0
-                )
+            withTimeout(10_000) {
+                val data = coinGecko.getCoinById(id= coinId, marketData = true)
+                flow{
+                    emit(
+                        DataDetailsCoin(
+                        name = data.name,
+                        priceCurrent = data.marketData?.currentPrice?.get(currencyString) ?: 0.0,
+                        image = data.image,
+                        priceChangePercentage24h = data.marketData?.priceChangePercentage24h ?: 0.0,
+                        priceChangePercentage7d = data.marketData?.priceChangePercentage7d ?: 0.0,
+                        circulatingSupply = data.marketData?.circulatingSupply ?: 0.0,
+                        high24h = data.marketData?.high24h?.get(currencyString) ?: 0.0,
+                        low24h = data.marketData?.low24h?.get(currencyString) ?: 0.0,
+                        marketCap = data.marketData?.marketCap?.get(currencyString) ?: 0.0,
+                        marketCapChangePercentage24h = data.marketData?.marketCapChangePercentage24h ?: 0.0
+                    ))
+                }
+
             }
         } catch (e: Exception) {
             throw CoinGeckoExceptionError("Retrieval of details of the coin was unsuccessful")
