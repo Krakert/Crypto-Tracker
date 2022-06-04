623c107fcd2be702887e98bb5463f3715e3c62b4
Stefan de Kraker <stefandekraker1999@gmail.com>
2022-06-04 16:10:29 +0200

    Setup of the Tile, but don't wanna spent my time on it. We need to wait at Glance

--- app/src/main/java/com/krakert/tracker/MainActivity.kt
+++ app/src/main/java/com/krakert/tracker/MainActivity.kt
@@ -1,19 +1,38 @@
 package com.krakert.tracker
 
+import android.content.ComponentName
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
+import androidx.wear.tiles.manager.TileUiClient
+import com.krakert.tracker.databinding.ActivityMainBinding
 import com.krakert.tracker.navigation.NavGraph
+import com.krakert.tracker.tile.CoinTileService
+
 
 class MainActivity : ComponentActivity() {
 
+    private lateinit var tileUiClient: TileUiClient
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(R.layout.activity_main)
 
         setContent {
             TrackerMain()
         }
+
+        // Enable this for tile
+//        tileUiClient = TileUiClient(
+//            context = this,
+//            component = ComponentName(this,  CoinTileService::class.java),
+//            parentView = binding.root
+//        )
+//        tileUiClient.connect()
+
     }
 
     @Composable
