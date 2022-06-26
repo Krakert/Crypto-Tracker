2bceeb4c147bc19ce979f40a1e497e4659a21e3e
Stefan de Kraker <stefandekraker1999@gmail.com>
2022-06-26 21:55:53 +0200

    Return a List<Coin> instead of Coins

--- app/src/main/java/com/krakert/tracker/repository/FirebaseRepository.kt
+++ app/src/main/java/com/krakert/tracker/repository/FirebaseRepository.kt
@@ -1,6 +1,7 @@
 package com.krakert.tracker.repository
 
 import com.google.firebase.firestore.FirebaseFirestore
+import com.krakert.tracker.model.Coin
 import com.krakert.tracker.model.Coins
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
@@ -11,16 +12,23 @@ class FirebaseRepository {
     private var firestore: FirebaseFirestore = FirebaseFirestore.getInstance()
     private var documentCoins = firestore.collection("Tracker").document("is3ksOVRTraukL5sSbgJ")
 
-    suspend fun getListCoins(): Flow<Coins> {
+    suspend fun getListCoins(): Flow<List<Coin>> {
         return try {
             withTimeout(10_000) {
                 val result = documentCoins.get().await()
+                val time = System.currentTimeMillis()
+                val list = result.toObject(Coins::class.java)?.Coins
+                list?.forEach {
+                    it.timeStamp = time
+                }
                 flow {
-                    result.toObject(Coins::class.java)?.let { emit(it) }
+                    if (list != null) {
+                        emit(list)
+                    }
                 }
             }
         } catch (e: Exception) {
-            throw FireBaseExceptionError("Retrieval firebase was unsuccessful")
+            throw FireBaseExceptionError("Retrieval firebase was unsuccessful: $e")
         }
     }
 
