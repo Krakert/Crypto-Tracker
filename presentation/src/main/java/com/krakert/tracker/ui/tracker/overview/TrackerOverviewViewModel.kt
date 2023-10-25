e60ce770ba3bc8b7fecc71e005c9fa558495089a
Stefan de Kraker <stefandekraker1999@gmail.com>
2023-10-25 15:30:19 +0200

    Fixed small UI flow problem

--- presentation/src/main/java/com/krakert/tracker/ui/tracker/overview/TrackerOverviewViewModel.kt
+++ presentation/src/main/java/com/krakert/tracker/ui/tracker/overview/TrackerOverviewViewModel.kt
@@ -8,6 +8,10 @@ import androidx.lifecycle.viewModelScope
 import com.krakert.tracker.domain.tracker.GetFavouriteCoins
 import com.krakert.tracker.domain.tracker.GetOverview
 import com.krakert.tracker.ui.tracker.model.ProblemState
+import com.krakert.tracker.ui.tracker.model.ProblemState.EMPTY
+import com.krakert.tracker.ui.tracker.model.ProblemState.NO_CONNECTION
+import com.krakert.tracker.ui.tracker.model.ProblemState.SSL
+import com.krakert.tracker.ui.tracker.model.ProblemState.UNKNOWN
 import com.krakert.tracker.ui.tracker.overview.ViewStateOverview.Loading
 import com.krakert.tracker.ui.tracker.overview.ViewStateOverview.Problem
 import com.krakert.tracker.ui.tracker.overview.ViewStateOverview.Success
@@ -17,7 +21,6 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
-import timber.log.Timber
 import java.net.ConnectException
 import javax.inject.Inject
 import javax.net.ssl.SSLHandshakeException
@@ -45,149 +48,38 @@ class OverviewViewModel
             mutableStateOverview.emit(Loading)
             getFavouriteCoins()
                 .onSuccess { listFavouriteCoins ->
-                    if (listFavouriteCoins.result.isEmpty()) {
-                        mutableStateOverview.emit(Problem(ProblemState.EMPTY))
-                    }
-                    Timber.i(listFavouriteCoins.result.toString())
-                    getOverview()
-                        .onSuccess { coinOverview ->
-                            mutableStateOverview.emit(
-                                Success(
-                                    overviewCoinDisplayMapper.map(coinOverview)
+                    if (listFavouriteCoins.result.isNotEmpty()) {
+                        getOverview()
+                            .onSuccess { coinOverview ->
+                                mutableStateOverview.emit(
+                                    Success(
+                                        overviewCoinDisplayMapper.map(coinOverview)
+                                    )
                                 )
-                            )
-                        }
-                        .onFailure {
-                            when (it) {
-                                is SSLHandshakeException -> {
-                                    mutableStateOverview.emit(Problem(ProblemState.SSL))
-                                }
+                            }
+                            .onFailure {
+                                when (it) {
+                                    is SSLHandshakeException -> {
+                                        mutableStateOverview.emit(Problem(SSL))
+                                    }
 
-                                is ConnectException -> {
-                                    mutableStateOverview.emit(Problem(ProblemState.NO_CONNECTION))
-                                }
+                                    is ConnectException -> {
+                                        mutableStateOverview.emit(Problem(NO_CONNECTION))
+                                    }
 
-                                else -> {
-                                    mutableStateOverview.emit(Problem(ProblemState.UNKNOWN))
+                                    else -> {
+                                        mutableStateOverview.emit(Problem(UNKNOWN))
+                                    }
                                 }
                             }
-                        }
+                    } else {
+                        mutableStateOverview.emit(
+                            Problem(EMPTY)
+                        )
+                    }
                 }
         }
     }
-
-//        val favoriteCoinList = sharedPreferences.getFavoriteCoinList()
-//
-//        if (favoriteCoinList.isNotEmpty()) {
-//            viewModelScope.launch {
-//                if (!cryptoRepository.isOnline()) {
-//                    _viewState.value = ViewStateOverview.Problem(ProblemState.NO_CONNECTION)
-//                } else {
-//                    cryptoRepository.getPriceCoins(
-//                        idCoins = getCoinsIdString(favoriteCoinList),
-//                        currency = sharedPreferences.Currency.toString()
-//                    ).collect { result ->
-//                        when (result) {
-//                            is Resource.Success -> {
-//                                //All out success
-//                                val overviewMergedCoinData =
-//                                    getHistoryForCoins(favoriteCoinList, result)
-//                                _viewState.value = if (overviewMergedCoinData.isNotEmpty()) {
-//                                    ViewStateOverview.Success(overviewMergedCoinData)
-//                                } else {
-//                                    ViewStateOverview.Problem(ProblemState.COULD_NOT_LOAD)
-//                                }
-//                            }
-//
-//                            is Resource.Error -> {
-//                                _viewState.value = ViewStateOverview.Problem(result.state)
-//                            }
-//
-//                            is Resource.Loading -> {
-//                                _viewState.value = ViewStateOverview.Loading
-//                            }
-//                        }
-//                    }
-//                }
-//            }
-//        } else {
-//            _viewState.value = ViewStateOverview.Problem(ProblemState.EMPTY)
-//        }
-//    }
-
-//    private suspend fun getHistoryForCoins(
-//        listCoins: ArrayList<FavoriteCoin>,
-//        priceCoin: Resource<Map<String, MutableMap<String, Any>>>,
-//    ) : ArrayList<OverviewMergedCoinData> {
-//        val timestamp = System.currentTimeMillis()
-//        val overviewCoinList = arrayListOf<OverviewMergedCoinData>()
-//
-//        listCoins.forEach { item ->
-//            cryptoRepository.getHistoryByCoinId(
-//                coinId = item.id,
-//                currency = sharedPreferences.Currency.toString(),
-//                days = sharedPreferences.AmountDaysTracking.toString()
-//            ).collect { graphData ->
-//                when (graphData) {
-//                    is Resource.Success -> {
-//                        graphData.data?.prices?.let { marketChart ->
-//                            priceCoin.data?.get(item.id)?.put("market_chart", marketChart)
-//                        }
-//                        priceCoin.data?.get(item.id)?.put("time_stamp", timestamp)
-//
-//                        val dataChart = buildGraphData(graphData)
-//
-//                        val currency = sharedPreferences.Currency
-//
-//                        overviewCoinList.add(
-//                            OverviewMergedCoinData(
-//                                id = item.id,
-//                                name = item.name,
-//                                priceCoin.data?.get(item.id)?.get(currency).toString(),
-//                                graphData = dataChart,
-//                                timestamp = timestamp
-//                            )
-//                        )
-//                    }
-//                    is Resource.Error -> {
-//                        Log.e("HistoryItem", "History for $item failed")
-//                    }
-//                    else -> {
-//                        Log.e("HistoryItem", "Not a valid resource state triggered")
-//                    }
-//                }
-//            }
-//        }
-//
-//        return overviewCoinList
-//    }
-
-//    private fun buildGraphData(graphData: Resource<MarketChart>): ArrayList<GraphItem> {
-//        val dataChart = arrayListOf<GraphItem>()
-//        graphData.data?.prices?.forEach { datapoint ->
-//            dataChart.add(
-//                GraphItem(
-//                    price = datapoint[1],
-//                    timestamp = datapoint[0].toLong()
-//                )
-//            )
-//
-//        }
-//        return dataChart
-//    }
-
-    //TODO: StringBuilder might be nice
-//    private fun getCoinsIdString(
-//        listCoins: ArrayList<FavoriteCoin>,
-//    ) : String {
-//        val idCoins = arrayListOf<String>()
-//
-//        listCoins.forEach {
-//            idCoins.add(it.id)
-//        }
-//
-//        return idCoins.joinToString(",")
-//    }
 
     fun openSettings() {
         val intent = Intent(Settings.ACTION_DATE_SETTINGS)
