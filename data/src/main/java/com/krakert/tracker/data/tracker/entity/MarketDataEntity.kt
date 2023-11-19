79326110d0e56e1d08207526332df853a205f8e8
Stefan de Kraker <stefandekraker1999@gmail.com>
2023-11-19 19:09:20 +0100

    Changed naming

--- data/src/main/java/com/krakert/tracker/data/tracker/entity/MarketDataEntity.kt
+++ data/src/main/java/com/krakert/tracker/data/tracker/entity/MarketDataEntity.kt
@@ -1,17 +1,18 @@
 package com.krakert.tracker.data.tracker.entity
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class MarketDataEntity(
-    val current_price: Map<String, Double>?,
-    val market_cap: Map<String, Double>?,
-    val high_24h: Map<String, Double>?,
-    val low_24h: Map<String, Double>?,
-    val price_change_24h_in_currency: Map<String, Double>?,
-    val price_change_percentage_24h: Double?,
-    val price_change_percentage_7d: Double?,
-    val market_cap_change_percentage_24h_in_currency: Map<String, Double>?,
-    val circulating_supply: Double?,
-    val last_updated: String?,
+    @SerialName("current_price") val currentPrice: Map<String, Double>?,
+    @SerialName("market_cap") val marketCap: Map<String, Double>?,
+    @SerialName("high_24h") val high24h: Map<String, Double>?,
+    @SerialName("low_24h") val low24h: Map<String, Double>?,
+    @SerialName("price_change_24h_in_currency") val priceChange24hInCurrency: Map<String, Double>?,
+    @SerialName("price_change_percentage_24h") val priceChangePercentage24h: Double?,
+    @SerialName("price_change_percentage_7d") val priceChangePercentage7d: Double?,
+    @SerialName("market_cap_change_percentage_24h_in_currency") val marketCapChangePercentage24hInCurrency: Map<String, Double>?,
+    @SerialName("circulating_supply") val circulatingSupply: Double?,
+    @SerialName("last_updated") val lastUpdated: String?,
 )
