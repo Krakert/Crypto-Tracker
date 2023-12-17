bbf9504f2b26ec1010d1d1c3512dc60e234e4f31
Stefan de Kraker <stefandekraker1999@gmail.com>
2023-12-17 21:12:43 +0100

    Updated Logging to use SLF4J

--- data/src/main/java/com/krakert/tracker/data/di/HttpClientFactory.kt
+++ data/src/main/java/com/krakert/tracker/data/di/HttpClientFactory.kt
@@ -4,12 +4,11 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import io.github.aakira.napier.DebugAntilog
-import io.github.aakira.napier.Napier
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.android.Android
 import io.ktor.client.plugins.HttpCallValidator
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.logging.DEFAULT
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
@@ -37,12 +36,8 @@ object HttpClientFactory {
             )
         }
         install(Logging) {
-            logger = object : Logger {
-                override fun log(message: String) {
-                    Napier.d(message, tag = "HTTP Client")
-                }
-            }
-            level = LogLevel.INFO
-        }.also { Napier.base(DebugAntilog()) }
+            logger = Logger.DEFAULT
+            level = LogLevel.BODY
+        }
     }
 }
