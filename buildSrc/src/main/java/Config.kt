c64a947898fa28f7251acb7c8ed0030fa4e4010b
Stefan de Kraker <stefandekraker1999@gmail.com>
2024-01-12 16:51:39 +0100

    Update for release 11

--- buildSrc/src/main/java/Config.kt
+++ buildSrc/src/main/java/Config.kt
@@ -7,7 +7,7 @@ object Config {
 
     const val applicationId = "com.krakert.tracker"
     const val versionName = "Clean"
-    const val versionCode = 10
+    const val versionCode = 11
 
 
 }
--- presentation/src/test/java/com/krakert/tracker/PriceFormatterTest.kt
+++ presentation/src/test/java/com/krakert/tracker/PriceFormatterTest.kt
@@ -11,23 +11,23 @@ class PriceFormatterTest {
 
     @Test
     fun currentPriceFormatter_Price_Below_1() {
-        assertEquals("0.90447", formatter.map(0.9044717858868634))
+        assertEquals("0.90447", formatter.format(0.9044717858868634))
     }
 
     @Test
     fun currentPriceFormatter_Price_Above_1() {
-        assertEquals("1.7458", formatter.map(1.7457817858868634))
+        assertEquals("1.7458", formatter.format(1.7457817858868634))
     }
 
     @Test
     fun currentPriceFormatter_Price_Above_10() {
-        assertEquals("12.431", formatter.map(12.431243776886863))
+        assertEquals("12.431", formatter.format(12.431243776886863))
     }
 
     @Test
     fun currentPriceFormatter_Price_Above_100() {
         assertAll("price_above_100",
-            { assertEquals("38.568,19", formatter.map(38568.18920568336)) },
-            { assertEquals("149,99", formatter.map(149.9893872184712)) })
+            { assertEquals("38.568,19", formatter.format(38568.18920568336)) },
+            { assertEquals("149,99", formatter.format(149.9893872184712)) })
     }
 }
