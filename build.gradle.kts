88f13c8ea1fc4cad9ee1b3ee2c6fbce7c9cb4499
Stefan de Kraker <stefandekraker1999@gmail.com>
2024-03-21 17:52:43 +0100

    Update gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.1.4" apply false
+    id("com.android.application") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version Versions.kotlin apply false
     id("com.google.dagger.hilt.android") version Versions.daggerHilt apply false
     id("org.jetbrains.kotlin.plugin.serialization") version Versions.serialization apply false
