c4ddc18817879d3fc93b656e67ba1c616f550c63
Stefan de Kraker <stefandekraker1999@gmail.com>
2024-03-25 10:17:09 +0100

    Removed old sealed class ViewStateSettings

--- presentation/src/main/java/com/krakert/tracker/ui/tracker/settings/TrackerSettingsViewModel.kt
+++ presentation/src/main/java/com/krakert/tracker/ui/tracker/settings/TrackerSettingsViewModel.kt
@@ -7,10 +7,10 @@ import com.krakert.tracker.domain.tracker.ResetPreferences
 import com.krakert.tracker.domain.tracker.SetAmountDaysTracking
 import com.krakert.tracker.domain.tracker.SetCacheRateTime
 import com.krakert.tracker.domain.tracker.SetPreferencesCurrency
+import com.krakert.tracker.ui.components.ContentState
+import com.krakert.tracker.ui.components.OnDisplay
+import com.krakert.tracker.ui.components.OnLoading
 import com.krakert.tracker.ui.tracker.model.Currency
-import com.krakert.tracker.ui.tracker.model.ProblemState
-import com.krakert.tracker.ui.tracker.settings.ViewStateSettings.Loading
-import com.krakert.tracker.ui.tracker.settings.ViewStateSettings.Success
 import com.krakert.tracker.ui.tracker.settings.mapper.PreferencesDisplayMapper
 import com.krakert.tracker.ui.tracker.settings.model.SettingsDisplay
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -19,12 +19,6 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-sealed class ViewStateSettings {
-    // Represents different states for the overview screen
-    object Loading : ViewStateSettings()
-    data class Success(val data: SettingsDisplay) : ViewStateSettings()
-    data class Problem(val exception: ProblemState) : ViewStateSettings()
-}
 
 @HiltViewModel
 class TrackerSettingsViewModel
@@ -37,13 +31,13 @@ class TrackerSettingsViewModel
     private val preferencesDisplayMapper: PreferencesDisplayMapper
 ) : ViewModel() {
 
-    private val mutableStateSettings = MutableStateFlow<ViewStateSettings>(Loading)
+    private val mutableStateSettings = MutableStateFlow<ContentState<SettingsDisplay>>(OnLoading)
     val settingViewState = mutableStateSettings.asStateFlow()
 
     fun getSettings() {
         viewModelScope.launch {
             mutableStateSettings.emit(
-                Success(
+                OnDisplay(
                     preferencesDisplayMapper.map(getAllPreferences())
                 )
             )
